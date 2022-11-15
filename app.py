d39b123d8b8204112618383b53037fd5906f88b8
ikuto32 <ikuto0894@gmail.com>
2022-11-15 19:29:02 +0900

    パッケージリストを追加

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+from script import util
 
 app = Flask(__name__, static_folder='')
+
+class ItemList:
+    def __init__(self, image_dir:str) -> None:
+        self.imagesFiles = util.getImageFiles(image_dir=image_dir)
+        self.item = {file : 0.0 for file in self.imagesFiles}
+    
+    def setScore(self, item_name:str, score:float):
+        
+        self.item[item_name] = score
+    
+    def getScore(self, item_name:str):
+        return self.item.get(item_name)
+    
+    def getimagesFiles(self):
+        return self.imagesFiles
+
+
+itemlist = ItemList(image_dir="images")
 
 @app.route('/')
 def index():
@@ -9,5 +28,11 @@ def index():
 
     return text
 
+@app.route("/score")
+def getScore():
+    args = request.args
+    return itemlist.getScore(args.get("metaName"))
+
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
+
--- templates/js/main.js
+++ templates/js/main.js
@@ -1,13 +1,17 @@
-new Veu({
+var app = new Vue({
+	el: '#app',
+	data: {
+	  message: 'Hello Vue!'
+	}
+})
+
+new Vue({
 	el:"#img",
 	methods:{
 		getimg: function(){
 			url = "images/0.png"
 			axios.get(url)
-			.then(res => alert("get"))
+			.then(res => alert(res))
 		},
-		
 	}
-	
-
 })
