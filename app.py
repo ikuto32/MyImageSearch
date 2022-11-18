b10d8899d01a9d7d8490bc4ea8f562334468ae5f
ikuto32 <ikuto0894@gmail.com>
2022-11-18 11:24:57 +0900

    画像の表示を実装

--- app.py
+++ app.py
@@ -1,22 +1,34 @@
-from flask import Flask, request
+import io
+from flask import Flask, request, jsonify
+from PIL import Image
+from io import BytesIO
 from script import util
 
 app = Flask(__name__, static_folder='')
 
 class ItemList:
     def __init__(self, image_dir:str) -> None:
+        self.imagesDir = image_dir
         self.imagesFiles = util.getImageFiles(image_dir=image_dir)
-        self.item = {file : 0.0 for file in self.imagesFiles}
+        self.items = {file : 0.0 for file in self.imagesFiles}
     
     def setScore(self, item_name:str, score:float):
-        
-        self.item[item_name] = score
+        self.items[item_name] = score
     
     def getScore(self, item_name:str):
-        return self.item.get(item_name)
+        return self.items.get(item_name)
     
     def getimagesFiles(self):
         return self.imagesFiles
+    
+    def sortScore(self):
+        return sorted(self.items, key=lambda x:x[1])
+
+    def getFromOrder(self, order:int):
+        return list(self.items.keys())[order]
+
+    def getItemsFromOrder(self, min:int, max:int):
+        return dict(list(self.items.items())[min:max])
 
 
 itemlist = ItemList(image_dir="images")
@@ -31,8 +43,19 @@ def index():
 @app.route("/score")
 def getScore():
     args = request.args
-    return itemlist.getScore(args.get("metaName"))
+    return itemlist.getScore(item_name=args.get("metaName"))
 
+@app.route("/image")
+def getImage():
+    args = request.args
+    response = itemlist.getFromOrder(order=int(args.get("order")))
+    return response
+
+@app.route("/images")
+def getItems():
+    args = request.args
+    response = itemlist.getItemsFromOrder(int(args.get("min")), int(args.get("max")))
+    return jsonify(response)
 if __name__ == "__main__":
     app.run(debug=True)
 
--- templates/js/main.js
+++ templates/js/main.js
@@ -6,12 +6,34 @@ var app = new Vue({
 })
 
 new Vue({
-	el:"#img",
+	el:"#itemArea",
+	data:{
+		message: "test",
+		count: 0,
+		items:[
+			{
+				img: "images/0.png",
+				score: 0
+			},
+			{
+				img: "images/1.png",
+				score: 0.55
+			}
+		]
+	},
 	methods:{
-		getimg: function(){
-			url = "images/0.png"
-			axios.get(url)
-			.then(res => alert(res))
+		getimgs: function (){
+			console.log(this.count+" to "+(this.count+10))
+			axios.get("/images?min="+this.count+"&max="+(this.count+10))
+			.then(response => {
+				console.log(response.data)
+				for (var metaName in response.data){
+					score = response.data[metaName]
+					console.log(score)
+					this.items.push({img: "images/"+metaName, score: score})
+				}
+			});
+			this.count += 10
 		},
 	}
 })
