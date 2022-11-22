8e64b8148a89a14b64094d93326edaa7a87223bb
ikuto32 <ikuto0894@gmail.com>
2022-11-22 23:43:38 +0900

    スクロールバーの値と最大値を取得し、その差によって、先を取得する機能を作成。

--- templates/js/main.js
+++ templates/js/main.js
@@ -8,11 +8,38 @@ new Vue({
 		isShowSetting: false,
 		isRegexp: false,
 		image_size: 250,
+		scrollY: 0,
 		items:[
 
 		]
 	},
+	mounted() {
+		window.addEventListener("scroll", this.getScroll)
+    },
 	methods:{
+		getScroll() {
+
+			// ページ全体の高さ
+			let pageHeight = document.body.clientHeight;
+
+			// スクロールバー等を含まないブラウザの表示領域
+			let viewportHeight = document.documentElement.clientHeight;
+			
+			// スクロールの最大値
+			let scrollMaxY = pageHeight - viewportHeight;
+
+			// 現在のスクロール値
+			let scrollY = window.pageYOffset;
+
+			//console.log("スクロール: " + scrollY + " / " + scrollMaxY);
+
+			//最後に近づいたら、更新
+			if(scrollMaxY - scrollY < 20)
+			{
+				this.getimgs();
+			}
+
+        },
 		getimgs: function (){
 			console.log(this.count+" to "+(this.count+this.load_size))
 			axios.get("/images?min="+this.count+"&max="+(this.count+this.load_size))
@@ -53,6 +80,18 @@ new Vue({
 			param = {"trigger" : "NameSearch", "text" : this.text, "trueRegexp" : this.isRegexp.toString()}
 			axios.post("/search", param)
 		},
-		
+		copyImagesButton: function () {
+			let selected_images = []
+			for (var i in this.items){
+				item = this.items[i]
+				console.log(item.selected)
+				if (item.selected){
+					selected_images.push(item.img_name)
+				}
+			}
+			param = {"trigger" : "copyImages", "meta_names" : selected_images.join(',')}
+			console.log(param)
+			axios.post("/search", param)
+		},
 	}
 })
