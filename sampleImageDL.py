eab70de282c136fc2296b2778e28b66fae4f9c94
ikuto32 <ikuto0894@gmail.com>
2023-02-06 04:00:08 +0900

    サンプル画像を増やす

--- sampleImageDL.py
+++ sampleImageDL.py
@@ -1,20 +1,18 @@
-import os
-import pprint
 import time
 import urllib.error
 import urllib.request
+import tqdm
 
 def main():
-    for i in range(100):
+    for i in tqdm.tqdm(range(1000)):
         try:
             with urllib.request.urlopen('https://source.unsplash.com/random') as web_file:
                 data = web_file.read()
-                print()
                 with open(f"images/{i}.png", mode='wb') as local_file:
                     local_file.write(data)
         except urllib.error.URLError as e:
             print(e)
-        time.sleep(2.0)
+        time.sleep(1.0)
 
 if __name__ == "__main__":
     main()
